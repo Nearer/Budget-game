583eff7034a9ad78a09c73a00c67cb03fe2968c3
Nearer <n_danso@hotmail.fr>
2018-08-24 16:32:25 -0400

    Finish variable on the the front end

--- budget_teaching.py
+++ budget_teaching.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, make_response
-
+import sim
 
 app = Flask(__name__)
 
@@ -12,11 +12,47 @@ app = Flask(__name__)
 @app.route('/budget',methods = ['GET', 'POST'])
 def budget():
 
+     if request.method == 'POST':
+
+        # Inputs from a submission 
+        nourriture = request.form['Nourriture']
+        loyer = request.form['Loyer']
+        internet = request.form['Internet']
+        electricite = request.form['Electricite']
+        cinema = request.form['Cinema']
+        jeux_videos = request.form['Jeux_videos']
+        restaurant = request.form['Restaurant']
+        f1 = request.form['F1']
+     
+     
      
         
+        
+
+        # call the simulation function 
+        economies,happy,mois = sim.happyness(nourriture,loyer,internet,electricite,cinema,jeux_videos,restaurant,f1)
+
+    # Initial values
+     mois_defaut=1
+     happy_defaut=100
+     economies_defaut=0
+    
+
+     if  'mois' in locals():
+         mois_defaut=mois
+     if 'happy' in locals():
+         happy_defaut=happy
+     if 'happy' in locals():
+         economies_defaut=economies
 
 
-     return render_template('index.html')
+
+
+
+
+
+     return render_template('index.html',economies=economies_defaut,mois=mois_defaut,happy=happy_defaut)
+
 
 
 
--- sim.py
+++ sim.py
@@ -3,15 +3,20 @@
 def happyness(nourriture,loyer,internet,electricite,cinema,jeu_video,restaurant,f1,economies=0,happy=100,mois=1):
     revenu=100
     mois=+mois
-    depense_nec=nourriture + loyer+internet+electricite
-    depense_fun=cinema + jeu_video+restaurant+f1
-    total=depense_fun + depense_nec
-    happy=happy-abs(0.75*revenu-depense_nec)-(0.25*revenu-depense_fun)+20
+    depense_nec = nourriture + loyer + internet + electricite
+    depense_fun= cinema + jeu_video + restaurant + f1
+    total= depense_fun + depense_nec
+    r_dep_fun=(0.25*revenu-depense_fun)+20
+    r_dep_nec=abs(0.75*revenu-depense_nec)
+    
+    happy=happy-r_dep_nec-r_dep_fun
     economies= economies + revenu - total
-    print("revenu=",revenu,"total=", total,"economies=", economies,"happy=",happy,"mois=",mois)
+    # print("revenu=",revenu,"total=", total,"economies=", economies,"happy=",happy,"mois=",mois)
     return economies,happy,mois
 
+if __name__ == "__main__":
+    # execute only if run as a script
+    happyness()
 
-
-happyness(10,30,5,5,4,2,9,15)
+# happyness(10,30,5,5,4,2,9,15)
 
