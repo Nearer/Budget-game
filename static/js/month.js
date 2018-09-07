89decf52c31e7daf06611fe40ddb7ac5dcb2c5a7
Nearer <n_danso@hotmail.fr>
2018-09-07 12:44:22 -0400

    add month iteration funtion

--- static/js/month.js
+++ static/js/month.js
@@ -1,7 +1,13 @@
+// Iterates months
+
 function  noMonth(){
 
     var count = localStorage.getItem("count")==null?1:localStorage.getItem("count");
     count=Number(count)+1;
     localStorage.setItem("count", count);
-    document.getElementById('month').innerHTML = count;
+    document.getElementById('month').innerHTML = count-1;
   }
+ 
+window.onload = function(){
+    noMonth()
+}; 
