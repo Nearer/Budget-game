53222d62e1dbcc85b50ced6dc1219bab3cb265fd
Nearer <n_danso@hotmail.fr>
2018-09-11 03:01:23 -0400

    Use javascript for treating computation

--- budget_teaching.py
+++ budget_teaching.py
@@ -1,62 +1,25 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, make_response
-import sim
+
 
 app = Flask(__name__)
 
-
-
+ 
+       
 
 
 
 @app.route('/',methods = ['GET', 'POST'])
-@app.route('/budget',methods = ['GET', 'POST'])
 def budget():
+  
+        #   
+              
+     
 
-     if request.method == 'POST':
+     return render_template('index.html')
 
-        # Inputs from a submission 
-        nourriture = int(request.form['Nourriture'])
-        loyer = int(request.form['Loyer'])
-        internet = int(request.form['Internet'])
-        electricite = int(request.form['Electricite'])
-        cinema = int(request.form['Cinema'])
-        jeux_videos = int(request.form['Jeux_videos'])
-        restaurant = int(request.form['Restaurant'])
-        f1 = int(request.form['F1'])
-     
-     
-     
+ 
+
         
-        
-
-        # call the simulation function 
-        economies,happy,dette = sim.happyness(nourriture,loyer,internet,electricite,cinema,jeux_videos,restaurant,f1)
-
-    # Initial values
-     mois_defaut=1
-     happy_defaut=100
-     economies_defaut=0
-     dette_defaut=0
-
-     if  'mois' in locals():
-         mois_defaut=mois
-     if 'happy' in locals():
-         happy_defaut=happy
-     if 'economies' in locals():
-         economies_defaut=economies
-     if 'dette'in locals():
-         dette_defaut=dette
-
-
-
-
-
-
-     return render_template('index.html',economies=economies_defaut,mois=mois_defaut,happy=happy_defaut,dette=dette_defaut)
-
-
-
-
 
 
 if __name__ == '__main__':
--- static/js/month.js
+++ static/js/month.js
@@ -1,26 +1,116 @@
-// Iterates months
+
 
 function  noMonth(){
 
-    var count = localStorage.getItem("count")==null?1:localStorage.getItem("count");
+    var count = localStorage.getItem("count")==null?0:localStorage.getItem("count");
     count=Number(count)+1;
     localStorage.setItem("count", count);
-    document.getElementById('month').innerHTML = count-1;
-  }
- 
-window.onload = function(){
-    // CALL FUNTION TO ITERATE MONTHS
-    noMonth()
-    
-    
-    // SHOW MODAL ON THE FIRST MONTH
-    if(localStorage.getItem("count") -1 == 1 ){
-        $('#myModal').modal('show');
+    document.getElementById('month').innerHTML = count;
     }
-    
+
+function valeur_i() {  
+
+    if(localStorage.getItem("economies") !== null){
+     economies = Number(localStorage.getItem("economies"));
+    } else {
+     economies = 0;
+    }
+
+    if(localStorage.getItem("dette") !== null){
+     dette = Number(localStorage.getItem("dette"));
+    } else {
+     dette = 0;
+    }
+
+    if(localStorage.getItem("happy") !== null){
+     happy = Number(localStorage.getItem("happy"));
+    } else {
+     happy = 100;
+    }
+
+    document.getElementById('economies').innerHTML = economies;
+    document.getElementById('dette').innerHTML = dette;
+    document.getElementById('happy').innerHTML = happy + '%';
+}
+
+// CODE FOR BUDGET COMPUTATION
+
+function sim(){
+
+
+    var Nourriture = Number(document.querySelector('input[name="Nourriture"]:checked').value);
+    var Loyer = Number(document.querySelector('input[name="Loyer"]:checked').value);
+    var Internet = Number(document.querySelector('input[name="Internet"]:checked').value);
+    var Electricite =Number(document.querySelector('input[name="Electricite"]:checked').value);
+    var Cinema = Number(document.querySelector('input[name="Cinema"]:checked').value);
+    var Jeux_videos = Number( document.querySelector('input[name="Jeux_videos"]:checked').value);
+    var Restaurant = Number(document.querySelector('input[name="Restaurant"]:checked').value);
+    var F1 = Number( document.querySelector('input[name="F1"]:checked').value); 
+
+    Nourriture*=10;
+    Loyer*=30;
+    Internet*=5;
+    Electricite*=5
+    Cinema*=4;
+    Jeux_videos*=2;
+    Restaurant*=9;
+    F1*=15;
+
+
+
+    var revenu = 100;
+
+    var depense_nec =  Nourriture + Loyer + Internet + Electricite;
+
+
+    var depense_fun= Cinema + Jeux_videos + Restaurant + F1;
+
+
+    var total = depense_fun + depense_nec;
+
+    var r_dep_fun= 30 - depense_fun;
+    var r_dep_nec= 50 - depense_nec;
+
+    economies = economies + revenu - total - dette;
+
+    if (economies < 0){
+        dette=Math.abs(economies);
+        economies=0;
+    } else {
+        dette = 0;
+    }
+
+    if (total <= 80){
+        if(happy == 100){    
+            happy = happy - r_dep_nec -  r_dep_fun ;
+        }else{
+            happy = happy - r_dep_nec -  r_dep_fun + 0.1 * economies ;
+        }
+    }
+    if (total > 80){
+        happy = happy - r_dep_fun - 2*dette -20;
+    }
+
+    if(happy>100){happy=100;}
+
+    localStorage.setItem("economies", economies);
+    localStorage.setItem("dette", dette);
+    localStorage.setItem("happy", happy);
+
+    }
+
+
+
+
+
+
+window.onload = function(){
+// CALL FUNTION TO ITERATE MONTHS
+noMonth()
+//  assign localstorage value for economies, dette, happy
+valeur_i()
+
 }; 
 
-// Modal
-
-// show modal
+document.getElementById("myBtn").addEventListener("click", sim);
 
